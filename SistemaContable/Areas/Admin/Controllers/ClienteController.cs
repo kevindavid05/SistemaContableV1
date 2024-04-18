660844ab9d0335fdb32e092681c73a07f96a91c9
DavidYol <David.yolp@gmail.com>
2024-04-18 13:02:37 -0600

    18042024

---
+++ SistemaContable/Areas/Admin/Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaContable.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaContable.Areas.Admin.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public ClienteController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Cliente.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        #endregion
+
+    }
+}
